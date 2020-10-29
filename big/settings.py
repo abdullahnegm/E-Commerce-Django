3dc7241c6c20143c87cb593dbd16de6ad02b8222
abdullahnegm <69604740+abdullahnegm@users.noreply.github.com>
2020-10-29 23:46:52 +0200

    Update settings.py

--- big/settings.py
+++ big/settings.py
@@ -3,14 +3,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = '*********************************'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')d8h%hfa9owug#i(i&s956g**zot*9cc&dt0c-8x*lc835w9u8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
